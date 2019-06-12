a9232e033b7399067aae3911c4c3913ee50a7303
Harshit Batra <payafterwork.in@gmail.com>
2019-06-12 19:22:06 +0530

    Meta Details and Pagination

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,6 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+              <div class="card-body">
+                This Q was published by {{$question->created_at->diffforHumans()}} by <a href="#">{{$question->creator->name}}</a> and currently has {{$question->answers()->count()}} answers.
+                </div>
 
                 <div class="card-body">
                     <div class="card-header">{{$question->creator->name}} asked
@@ -12,7 +15,10 @@
                      <p>{{$question->qdetails}}</p>
                      <hr>
                 </div>
+
                 <div class="card-body">
+                 
+                  <?php $anwers = $question->answers()->paginate(1); ?>
                     @foreach($question->answers as $answer)
                     <div class="card-header">
                         {{$answer->owner->name}} said {{$answer->created_at->diffForHumans()}}
@@ -21,6 +27,7 @@
                     <p>{{$answer->ans}}</p>
                     @endforeach
                     
+                    {{$anwers->links()}}
                 </div>
                 <div class="class-body">
                    @if (auth()->check())
