9b25b31d80672cdd21698d63d299bd45ec5211ab
Harshit Batra <payafterwork.in@gmail.com>
2019-07-19 11:05:59 +0530

    basic tweaks

---
+++ resources/views/questions/_list.blade.php
@@ -0,0 +1,15 @@
+<div class="card-body">
+                    @foreach($questions as $question)
+                    
+                    @if($question->hasUpdatesFor(auth()->user()))
+                     <h4><strong><a href="{{$question->path()}}">{{$question->qtitle}}</a></strong></h4>
+                    @else
+                    <h4><a href="{{$question->path()}}">{{$question->qtitle}}</a></h4>
+                    @endif 
+                     <strong><a href="{{$question->path()}}">{{$question->answers_count}}
+                     {{str_plural('answer',$question->answers_count)}}   
+                     </strong></a>
+                     <p>{{$question->qdetails}}</p>
+                     <hr>
+                    @endforeach
+                </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -6,23 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Questions</div>
-            
-                <div class="card-body">
-                    @foreach($questions as $question)
-                    
-                    @if($question->hasUpdatesFor(auth()->user()))
-                     <h4><strong><a href="{{$question->path()}}">{{$question->qtitle}}</a></strong></h4>
-                    @else
-                    <h4><a href="{{$question->path()}}">{{$question->qtitle}}</a></h4>
-                    @endif 
-                     <strong><a href="{{$question->path()}}">{{$question->answers_count}}
-                     {{str_plural('answer',$question->answers_count)}}   
-                     </strong></a>
-                     <p>{{$question->qdetails}}</p>
-                     <hr>
-                    @endforeach
-                </div>
-
+               @include('questions._list');
             </div>
         </div>
     </div>
